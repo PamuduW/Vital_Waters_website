bf1fb256ea54c4f433f0a809aeef34ea10d2911f
Pamudu Wijesingha <155348391+PamuduW@users.noreply.github.com>
2024-03-27 15:58:31 +0530

    test

--- Shop.js
+++ Shop.js
@@ -1,12 +1,18 @@
-let iconCart = document.querySelector('.icon-cart');
-let closeCart = document.querySelector('.close')
-let body = document.querySelector('body');
+let iconCart = document.querySelector(".icon-cart");
+let closeCart = document.querySelector(".close");
+let body = document.querySelector("body");
 
-iconCart.addEventListener('click', () =>{
-    body.classList.toggle('showCart')
-})
+let products = [
+  [1, "Product 1", 200, "Pictures\\Shop\\T-shirt_1.png"],
+  [1, "Product 1", 200, "Pictures\\Shop\\T-shirt_1.png"],
+  [1, "Product 1", 200, "Pictures\\Shop\\T-shirt_1.png"],
+];
 
-closeCart.addEventListener('click', () =>{
-    body.classList.toggle('showCart')
-})
+iconCart.addEventListener("click", () => {
+  body.classList.toggle("showCart");
+});
+
+closeCart.addEventListener("click", () => {
+  body.classList.toggle("showCart");
+});
 
