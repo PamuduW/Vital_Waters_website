c62de122d6631c110b8923b5ba94204d733b7d0e
Pamudu Wijesingha <155348391+PamuduW@users.noreply.github.com>
2024-03-27 23:06:39 +0530

    test

--- Shop.js
+++ Shop.js
@@ -2,7 +2,11 @@ let iconCart = document.querySelector(".icon-cart");
 let closeCart = document.querySelector(".close");
 let body = document.querySelector("body");
 let listProductHTML = document.querySelector(".listProduct");
-let products = [
+let listCartHTML = document.querySelector(".listCart");
+let iconCartSpan = document.querySelector(".icon-cart span");
+
+let carts = [];
+let listProducts = [
   {
     id: 1,
     name: "T-shirt 1",
@@ -63,9 +67,10 @@ closeCart.addEventListener("click", () => {
 
 const addDataToHTML = () => {
   listProductHTML.innerHTML = "";
-  products.forEach((product) => {
+  listProducts.forEach((product) => {
     let newProduct = document.createElement("div");
     newProduct.classList.add("item");
+    newProduct.dataset.id = product.id;
     newProduct.innerHTML =
       '<img src="' +
       product.image +
@@ -77,5 +82,65 @@ const addDataToHTML = () => {
     listProductHTML.appendChild(newProduct);
   });
 };
+
+listProductHTML.addEventListener("click", (event) => {
+  let positionClick = event.target;
+  if (positionClick.classList.contains("addCart")) {
+    let product_id = positionClick.parentElement.dataset.id;
+    addToCart(product_id);
+  }
+});
+
+const addToCart = (product_id) => {
+  let positionThisProductInCart = carts.findIndex(
+    (value) => value.product_id == product_id
+  );
+  if (carts.length <= 0) {
+    carts = [
+      {
+        product_id: product_id,
+        quantity: 1,
+      },
+    ];
+  } else if (positionThisProductInCart < 0) {
+    carts.push({
+      product_id: product_id,
+      quantity: 1,
+    });
+  } else {
+    carts[positionThisProductInCart].quantity += 1;
+  }
+  addCartToHTML();
+};
+
+const addCartToHTML = () => {
+  listCartHTML.innerHTML = "";
+  let totalQuantity = 0
+  if (carts.length > 0) {
+    carts.forEach((cart) => {
+      totalQuantity = totalQuantity + cart.quantity;
+      let newCart = document.createElement("div");
+      newCart.classList.add("item");
+      let positionProduct = listProducts.findIndex(
+        (value) => value.id == cart.product_id
+      );
+      let info = listProducts[positionProduct];
+      newCart.innerHTML =
+        '<div class="image"> <img src="' +
+        info.image +
+        '" alt="T-shirt_1" width="181px" height="200px"> </div> <div class="name">' +
+        info.name +
+        '</div> <div class="totalPrice">&#163;' +
+        (info.price * cart.quantity)+
+        '</div> <div class="quantity"> <span class="minus"></span> <span>' +
+        cart.quantity +
+        '</span> <span class="plus"></span> </div>';
+      listCartHTML.appendChild(newCart);
+    });
+  }
+  iconCartSpan.innerText = totalQuantity;
+};
+
+
 
 addDataToHTML();
