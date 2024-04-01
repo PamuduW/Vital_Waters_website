19b3627324d19c7d364e93fe7a3504aef196d3df
Pamudu Wijesingha <155348391+PamuduW@users.noreply.github.com>
2024-04-02 00:05:50 +0530

    test

--- profile.js
+++ profile.js
@@ -30,33 +30,60 @@ let answers = [
   //   ["", "", "", "", ""],
 ];
 
-let questiontype = [
+let questionType = [
   "<b>Personal Details</b>",
   "<b>Volunteering Tasks</b>",
   //   "<b>Qualifiations</b>",
   //   "<b>Avaiability and contact</b>",
 ];
 
-const data1 = document.getElementsByClassName("data1");
-const data2 = document.getElementsByClassName("data2");
-const data3 = document.getElementsByClassName("data3");
-const data4 = document.getElementsByClassName("data4");
+let questionPlaceHolder = [
+  [
+    "Name : ",
+    "Surname : ",
+    "Age : ",
+    "Gender (M/F) : ",
+    "Agree with privacy terms (Yes/No) : ",
+  ],
+  ["Rational : ", "DoA : ", "Task : ", "Place : ", "Assignment type : "],
+  //   [
+  //     "Area of study : ",
+  //     "Highest degree : ",
+  //     "University/Institution : ",
+  //     "Completion year : ",
+  //     "Country : ",
+  //   ],
+  //   [
+  //     "Availability for volunteering : ",
+  //     "Surname : ",
+  //     "Tel. No. : ",
+  //     "Email : ",
+  //     "Game on : ",
+  //   ],
+];
+
+const current_stage_name = document.getElementsByClassName("current_stage_name");
+const current_question = document.getElementsByClassName("current_question");
+const current_stage_progress = document.getElementsByClassName("current_stage_progress");
+
+const profile_progress = document.getElementsByClassName("profile_progress");
 
 const set1 = document.getElementsByClassName("set1");
 const set2 = document.getElementsByClassName("set2");
 const set3 = document.getElementsByClassName("set3");
 const set4 = document.getElementsByClassName("set4");
 
-const progress = document.getElementsByClassName("progress");
+const data1 = document.getElementsByClassName("data1");
+const data2 = document.getElementsByClassName("data2");
+const data3 = document.getElementsByClassName("data3");
+const data4 = document.getElementsByClassName("data4");
 
 let dataholder = [data1, data2, data3, data4];
 let setholder = [set1, set2, set3, set4];
-
 let i = 0,
-  j = 0,
-  k = 0;
+  j = 0;
 
-function gettingData() {
+function gettingTheAnswer() {
   if (j == questions[i].length) {
     alert("Please go to the next stage");
     return;
