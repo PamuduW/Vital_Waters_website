f7c4e757db199ca0ff64ef5c1af82dea265b5e1e
Pamudu Wijesingha <155348391+PamuduW@users.noreply.github.com>
2024-04-03 16:42:55 +0530

    test

--- profile.js
+++ profile.js
@@ -16,10 +16,10 @@ let questions = [
   ],
   [
     "Availability for volunteering : ",
-    "Surname : ",
+    "Address : ",
     "Tel. No. : ",
     "Email : ",
-    "Game on : ",
+    "Confirm all the above data is true (Yes/No) : ",
   ],
 ];
 
@@ -39,26 +39,26 @@ let questionType = [
 
 let questionPlaceHolder = [
   [
-    "Name : ",
-    "Surname : ",
-    "Age : ",
+    "Input firstname",
+    "Input surname : ",
+    "Input age",
     "Gender (M/F) : ",
     "Agree with privacy terms (Yes/No) : ",
   ],
   ["Rational : ", "DoA : ", "Task : ", "Place : ", "Assignment type : "],
   [
-    "Area of study : ",
-    "Highest degree : ",
-    "University/Institution : ",
-    "Completion year : ",
-    "Country : ",
+    "Input your area of study",
+    "What is your highest degree of education ",
+    "University/Institution",
+    "Completion year",
+    "Country",
   ],
   [
-    "Availability for volunteering : ",
-    "Surname : ",
+    "Availability for volunteering",
+    "Address",
     "Tel. No. : ",
     "Email : ",
-    "Game on : ",
+    "Confirm all the above data is true (Yes/No) :",
   ],
 ];
 
@@ -75,6 +75,40 @@ document.querySelector(".current_stage_name").innerHTML = `${questionType[i]}`;
 document.querySelector(".current_question").innerHTML = `${questions[i][j]}`;
 document.querySelector("input").placeholder = `${questionPlaceHolder[i][j]}`;
 
+function dataOut() {
+  document.querySelector(".current_question").innerHTML = `${questions[i][j]}`;
+  document.querySelector("input").placeholder = `${questionPlaceHolder[i][j]}`;
+  current_stage_progressbar = (j / questions[i].length) * 100;
+  document.querySelector(".current_stage_progress").innerHTML = `<div class="empty-progress-bar" style="width:80%">&nbsp;</div><div class="progress-bar" style="width: ${current_stage_progressbar}%;">${current_stage_progressbar}%</div>`;
+}
+
+function done() {
+  alert("All done!!!");
+  document.querySelector(".current_stage_name").innerHTML = `Thank you...`;
+  document.querySelector(".current_question").innerHTML = ``;
+  document.querySelector("input").placeholder = "";
+  document.querySelector(".current_stage_progress").style.display = "none";
+}
+
+function stageUpdate() {
+  document.querySelector(".current_stage_name").innerHTML = `${questionType[i]}`;
+  document.querySelector(".profile_progress").innerHTML = `<div class="empty-progress-bar" style="width:80%">&nbsp;</div> <div class="progress-bar" style="width: ${i * 20}%;">${i * 25}%</div>`;
+}
+
+function dataUpdate() {
+  if (j + 1 == questions[i].length) {
+    (i += 1), (j = 0);
+    stageUpdate();
+  } else {
+    j += 1;
+  }
+  if (i == questionType.length && j == 0) {
+    done();
+    return;
+  }
+  dataOut();
+}
+
 function gettingTheAnswer() {
   if (i == questionType.length && j == 0) {
     alert("All done!!! Thank you...");
@@ -82,66 +116,25 @@ function gettingTheAnswer() {
   } else if (document.querySelector("input").value == "") {
     alert("Please fill the blank");
     return;
-  } else if (j + 1 == questions[i].length) {
-    document.querySelector(".current_stage_name").innerHTML = `${
-      questionType[i + 1]
-    }`;
-    let profile_progressbar = ((i + 1) / questions.length) * 100;
-    document.querySelector(
-      ".profile_progress"
-    ).innerHTML = `<div class="empty-progress-bar" style="width:80%">.</div> <div class="progress-bar" style="width: ${
-      profile_progressbar - 20
-    }%;">${profile_progressbar}%</div>`;
   }
-  let temp = document.querySelector("input").value;
-  console.log(temp);
-  answers[i][j] = temp;
+  answers[i][j] = document.querySelector("input").value;
   dataholder[i][j].innerHTML = `${questions[i][j]} - ${answers[i][j]}`;
   document.querySelector(`.set${i + 1}`).style.display = "block";
-  j += 1;
   document.querySelector("input").value = "";
-  if (j == questions[i].length) {
-    (i += 1), (j = 0);
-  }
-  let current_stage_progressbar = (j / questions[i].length) * 100;
-  document.querySelector(
-    ".current_stage_progress"
-  ).innerHTML = `<div class="empty-progress-bar" style="width:80%">.</div><div class="progress-bar" style="width: ${current_stage_progressbar}%;">${current_stage_progressbar}%</div>`;
-  if (i == questionType.length && j == 0) {
-    alert("All done!!!");
-    document.querySelector(".current_stage_name").innerHTML = `Thank you...`;
-    document.querySelector(".current_question").innerHTML = ``;
-    document.querySelector("input").placeholder = "";
-    return;
-  }
-  document.querySelector(".current_question").innerHTML = `${questions[i][j]}`;
-  document.querySelector("input").placeholder = `${questionPlaceHolder[i][j]}`;
+  dataUpdate();
 }
 
 function goingToPreviousQuestion() {
   if (i == 0 && j == 0) {
-    alert("You are in first Question, can't go back!");
+    alert("You are in first Question, can't go back !!!");
     return;
   } else if (j == 0) {
     (i -= 1), (j = questions[i].length - 1);
-    let profile_progressbar = (i / questions.length) * 100;
-    document.querySelector(
-      ".profile_progress"
-    ).innerHTML = `<div class="empty-progress-bar" style="width:80%">.</div> <div class="progress-bar" style="width: ${
-      profile_progressbar - 20
-    }%;">${profile_progressbar}%</div>`;
+    stageUpdate();
   } else {
     j -= 1;
   }
-  document.querySelector(
-    ".current_stage_name"
-  ).innerHTML = `${questionType[i]}`;
-  document.querySelector(".current_question").innerHTML = `${questions[i][j]}`;
-  document.querySelector("input").placeholder = `${questionPlaceHolder[i][j]}`;
-  let current_stage_progressbar = (j / questions[i].length) * 100;
-  document.querySelector(
-    ".current_stage_progress"
-  ).innerHTML = `<div class="empty-progress-bar" style="width:80%">.</div><div class="progress-bar" style="width: ${current_stage_progressbar}%;">${current_stage_progressbar}%</div>`;
+  dataOut();
 }
 
 function skippingToNextQuestion() {
@@ -149,27 +142,8 @@ function skippingToNextQuestion() {
     alert("All done!!! Thank you...");
     return;
   }
-  document.querySelector(
-    ".current_stage_name"
-  ).innerHTML = `${questionType[i]}`;
   answers[i][j] = "Skipped";
-  dataholder[i][j].innerHTML = `${questions[i][j]} - ${answers[i][j]}`;
   document.querySelector(`.set${i + 1}`).style.display = "block";
-  if (j + 1 == questions[i].length) {
-    (i += 1), (j = 0);
-    let profile_progressbar = (i / questions.length) * 100;
-    document.querySelector(
-      ".profile_progress"
-    ).innerHTML = `<div class="empty-progress-bar" style="width:80%">.</div> <div class="progress-bar" style="width: ${
-      profile_progressbar - 20
-    }%;">${profile_progressbar}%</div>`;
-  } else {
-    j += 1;
-  }
-  document.querySelector(".current_question").innerHTML = `${questions[i][j]}`;
-  document.querySelector("input").placeholder = `${questionPlaceHolder[i][j]}`;
-  let current_stage_progressbar = (j / questions[i].length) * 100;
-  document.querySelector(
-    ".current_stage_progress"
-  ).innerHTML = `<div class="empty-progress-bar" style="width:80%">.</div> <div class="progress-bar" style="width: ${current_stage_progressbar}%;">${current_stage_progressbar}%</div>`;
+  dataholder[i][j].innerHTML = `${questions[i][j]} - ${answers[i][j]}`;
+  dataUpdate();
 }
